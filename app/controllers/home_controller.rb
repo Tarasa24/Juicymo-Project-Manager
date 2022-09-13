9debbd18144796d7e7a26158af9309d48663cd05
Tarasa24 <tarasa24@tarasa24.dev>
2022-09-13 23:04:06 +0200

    Add basic pages (Home, Dashboard, Profile)

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    # The home/index.html.erb view is rendered by default
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "home#index"
+  unauthenticated :user do
+    root 'home#index', as: :unauthenticated_root
+  end
+
+  authenticated :user do
+    root 'dashboard#index', as: :authenticated_root
+  end
+
+  get 'profile', to: 'profile#index'
 end
