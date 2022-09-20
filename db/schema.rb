559a33609da97d415d66e935a7e92f8af15b20b7
Tarasa24 <tarasa24@tarasa24.dev>
2022-09-20 15:05:41 +0200

    Remove redundant attachment column from tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_17_201915) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_130445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,7 +73,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_201915) do
     t.string "title", null: false
     t.text "description"
     t.boolean "is_done", null: false
-    t.string "attachment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "project_id"
